1cbd0c114b08d2eb954679b3f3553cff4836e0e2
Jamie Hardt <jhardt@soundepartment.com>
2012-10-03 16:27:54 -0700

    Working widgets to evaluate visual effect

--- JHWaveformView Demo/JHAppDelegate.h
+++ JHWaveformView Demo/JHAppDelegate.h
@@ -9,11 +9,24 @@
 #import <Cocoa/Cocoa.h>
 #import "JHWaveformView.h"
 
+enum testSignals {
+    sine = 1001,
+    square = 1002
+    };
+
 @interface JHAppDelegate : NSObject <NSApplicationDelegate> {
     JHWaveformView *_waveformView;
+
+    NSUInteger _numberOfSamples;
+    NSUInteger _testSignal;
+    
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (retain) IBOutlet JHWaveformView *waveformView;
+
+-(IBAction)setTestSignal:(id)sender;
+-(IBAction)setNumberOfSamples:(id)sender;
+
 
 @end
--- JHWaveformView Demo/JHAppDelegate.m
+++ JHWaveformView Demo/JHAppDelegate.m
@@ -12,12 +12,47 @@
 
 @synthesize waveformView = _waveformView;
 
+-(void)_setTestSignalToView {
+    
+    float *testSignal = malloc(_numberOfSamples * sizeof(float));
+    
+    NSUInteger i;
+    
+    if (_testSignal == sine) {
+        for (i = 0; i < _numberOfSamples ; i++) {
+            testSignal[i] = sinf(i);
+        }
+    } else if (_testSignal == square) {
+        for (i = 0; i < _numberOfSamples ; i++) {
+            testSignal[i] = (i % 2 - 0.5f) * 2;
+        }
+    }
+    
+    [_waveformView setWaveform:testSignal length:_numberOfSamples];
+    free(testSignal);
+}
+
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
     float testSignal[] = {0.0f,1.0f,-1.0f,1.0f,-1.0f,0.0f};
     [_waveformView setWaveform:testSignal length:6];
+    _numberOfSamples = 1000;
 
+}
+
+
+
+-(IBAction)setTestSignal:(id)sender {
+    _testSignal = [sender selectedTag];
+    [self _setTestSignalToView];
+}
+
+
+-(IBAction)setNumberOfSamples:(id)sender {
+    _numberOfSamples = [sender integerValue];
+    [self _setTestSignalToView];
 }
 
 @end
