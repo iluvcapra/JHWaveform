4e296ccd0757be37fa7dd0caeaca885c6235bcda
Jamie Hardt <jhardt@soundepartment.com>
2012-10-04 14:17:21 -0700

    A new subclass, JHAudioPReview

---
+++ JHWaveform/JHAudioPreview.h
@@ -0,0 +1,13 @@
+//
+//  JHAudioPreview.h
+//  JHWaveformView
+//
+//  Created by Jamie Hardt on 10/4/12.
+//  Copyright (c) 2012 Jamie Hardt. All rights reserved.
+//
+
+#import "JHWaveformView.h"
+
+@interface JHAudioPreview : JHWaveformView
+
+@end
---
+++ JHWaveform/JHAudioPreview.m
@@ -0,0 +1,14 @@
+//
+//  JHAudioPreview.m
+//  JHWaveformView
+//
+//  Created by Jamie Hardt on 10/4/12.
+//  Copyright (c) 2012 Jamie Hardt. All rights reserved.
+//
+
+#import "JHAudioPreview.h"
+
+@implementation JHAudioPreview
+
+
+@end
