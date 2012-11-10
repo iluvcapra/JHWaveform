07de148db4663b8cab447e8bc3ed621c60cba526
Jamie Hardt <jhardt@soundepartment.com>
2012-11-10 09:08:50 -0800

    rewrote some of this to make refactoring easier

--- JHWaveform/JHWaveformView.h
+++ JHWaveform/JHWaveformView.h
@@ -39,9 +39,14 @@ enum JHWaveformViewMouseModes {
 };
 
 @interface JHWaveformView : JHSignalView {
-    NSColor *_foregroundColor, *_lineColor, *_backgroundColor,
-    *_selectedColor, *_selectedBorderColor,*_gridColor;
+    NSColor     *_foregroundColor;
+    NSColor     *_backgroundColor;
+    NSColor     *_selectedColor;
+    NSColor     *_selectedBorderColor;
     
+    NSColor     *_gridColor;
+
+    NSColor     *_lineColor;
     CGFloat     _lineWidth;
     CGFloat     _verticalScale;
     
@@ -65,16 +70,16 @@ enum JHWaveformViewMouseModes {
  backgroundColor apply to the waveform itself.  By default, these colors are 
  set to neutral system-defined colors for NSCells */
 
-@property (copy, readwrite) NSColor *foregroundColor,
-                                    *lineColor,
-                                    *backgroundColor,
-                                    *selectedColor,
-                                    *selectedBorderColor,
-                                    *gridColor,
-                                    *rulerGradientBeginColor,
-                                    *rulerGradientEndColor,
-                                    *rulerTicksColor,
-                                    *outerBorderColor;
+@property (copy, readwrite) NSColor *foregroundColor;
+
+@property (copy, readwrite) NSColor *backgroundColor;
+@property (copy, readwrite) NSColor *selectedColor;
+@property (copy, readwrite) NSColor *selectedBorderColor;
+@property (copy, readwrite) NSColor *gridColor;
+@property (copy, readwrite) NSColor *lineColor;
+
+@property (copy, readwrite) NSColor *rulerTicksColor;
+@property (copy, readwrite) NSColor *outerBorderColor;
 
 /* This is the width of the waveform line the default is probably the best. */
 @property (assign) CGFloat lineWidth;
